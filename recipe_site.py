3a134f8a512cf77b20110df88ef716c11c6ffe1e
Venombane <63618043+Venombane@users.noreply.github.com>
2022-04-15 22:42:32 -0230

    Added the app routes for the 4 pages.

--- recipe_site.py
+++ recipe_site.py
@@ -6,7 +6,17 @@ app = Flask(__name__)
 def main_page():
     return render_template("mainPage.html")
 
-@app.route("/album")
-def album():
+@app.route("/addRecipe")
+def add_recipe():
     bicycle_images = ["Alice", "Bob", "Charlie"]
-    return render_template("index.html", bicycle_images=bicycle_images)
+    return render_template("addRecipe.html", bicycle_images=bicycle_images)
+
+@app.route("/recipeList")
+def recipes():
+    bicycle_images = ["Alice", "Bob", "Charlie"]
+    return render_template("recipeList.html", bicycle_images=bicycle_images)
+
+@app.route("/removeRecipe")
+def remove_recipe():
+    bicycle_images = ["Alice", "Bob", "Charlie"]
+    return render_template("removeRecipe.html", bicycle_images=bicycle_images)
