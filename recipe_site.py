171b1cd3c676823b906249bcd70ff4a0ccc1d91b
Venombane <63618043+Venombane@users.noreply.github.com>
2022-04-18 01:47:43 -0230

    Finished being able to export form elements into csv file and started work on displaying each recipe dynamically in recipeList.html.

--- recipe_site.py
+++ recipe_site.py
@@ -1,22 +1,41 @@
-from flask import Flask, render_template
+import csv
+
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def main_page():
     return render_template("mainPage.html")
 
-@app.route("/addRecipe")
+
+@app.route("/addRecipe", methods=['POST', 'GET'])
 def add_recipe():
-    bicycle_images = ["Alice", "Bob", "Charlie"]
-    return render_template("addRecipe.html", bicycle_images=bicycle_images)
+    if request.method == 'POST':
+        recipeName = request.form["recipe_name"]
+        servings = request.form['servings']
+        total = request.form['total_time']
+        prep = request.form['prep_time']
+        description = request.form['description']
+        ingredients = request.form['ingredients']
+        instructions = request.form['instructions']
+        recipe = [recipeName, servings, total, prep, description, ingredients, instructions]
+
+        with open('Recipes.csv', 'a') as file:
+            writer = csv.writer(file)
+            writer.writerow(recipe)
+        return render_template("recipeList.html")
+
+    else:
+        return render_template("addRecipe.html")
+
 
 @app.route("/recipeList")
 def recipes():
-    bicycle_images = ["Alice", "Bob", "Charlie"]
-    return render_template("recipeList.html", bicycle_images=bicycle_images)
+    return render_template("recipeList.html")
+
 
 @app.route("/removeRecipe")
 def remove_recipe():
-    bicycle_images = ["Alice", "Bob", "Charlie"]
-    return render_template("removeRecipe.html", bicycle_images=bicycle_images)
+    return render_template("removeRecipe.html")
